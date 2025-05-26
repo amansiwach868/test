e414a7dac853d059a0ed2759d19c6f029e44675a
amansiwach868 <amansiwach868@gmail.com>
2025-05-26 11:01:08 +0530

    NevBar-Responsive-Complete

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+document.getElementById("menuToggle").addEventListener("click", function () {
+    document.getElementById("menu").classList.toggle("show");
+    document.getElementById("span-1").classList.toggle("activeNavBar2");
+    document.getElementById("span-2").classList.toggle("activeNavBar3");
+    document.getElementById("span-3").classList.toggle("activeNavBar");
+});
