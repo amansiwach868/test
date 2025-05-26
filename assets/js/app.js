c9c672e5f3e6c1d1e816488e2f5dac9b1e8b996f
amansiwach868 <amansiwach868@gmail.com>
2025-05-26 12:35:42 +0530

    section-2-Responsive-Complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,24 @@ document.getElementById("menuToggle").addEventListener("click", function () {
     document.getElementById("span-2").classList.toggle("activeNavBar3");
     document.getElementById("span-3").classList.toggle("activeNavBar");
 });
+gsap.from(".nav-links", { y: -100, duration: 1, stagger: 0.09 });
+
+let tl = gsap.timeline()
+tl.from(".upperHeading", {
+    opacity: 0,
+    x: 200,
+    duration: 1,
+})
+gsap.from(".mainHeading", {
+    opacity: 0,
+    x: -200,
+    duration: 1,
+})
+tl.from(".headingPara", {
+    y: 20,
+    opacity: 0
+})
+tl.from(".headingBtn", {
+    y: 20,
+    opacity: 0
+})
