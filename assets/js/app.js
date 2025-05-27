09d3eb37f68bd78264fd5ddadff5feff68930ee1
amansiwach868 <amansiwach868@gmail.com>
2025-05-27 09:36:36 +0530

    All-complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 document.getElementById("menuToggle").addEventListener("click", function () {
     document.getElementById("menu").classList.toggle("show");
+    document.querySelector("body").classList.toggle("overflow-hidden");
     document.getElementById("span-1").classList.toggle("activeNavBar2");
     document.getElementById("span-2").classList.toggle("activeNavBar3");
     document.getElementById("span-3").classList.toggle("activeNavBar");
@@ -25,3 +26,15 @@ tl.from(".headingBtn", {
     y: 20,
     opacity: 0
 })
+const allAccordion = document.querySelectorAll(".accordion-items")
+allAccordion.forEach(element => {
+    element.addEventListener("click", () => {
+        allAccordion.forEach(item => {
+            if (item !== element) {
+                item.classList.remove("accordin");
+            }
+        });
+        element.classList.toggle("accordin")
+    })
+    
+});
